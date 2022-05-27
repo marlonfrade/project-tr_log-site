c7c86a2967909fe756d1aef1c5a324b9799b7fea
Marlon Frade <marlonmelo98@hotmail.com>
2022-05-27 17:50:20 -0300

    Developing the final layout part1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// **********************************************
+// ---------------ANTONIO------------------------
+// **********************************************
+
 function showTextSend() {
   document.getElementById("pick").removeAttribute("hidden");
   document.getElementById("trans").setAttribute("hidden", true);
@@ -15,6 +19,39 @@ function showTextReceive() {
   document.getElementById("pick").setAttribute("hidden", true);
   document.getElementById("trans").setAttribute("hidden", true);
 }
+
+// **********************************************
+// ----------------MARLON------------------------
+// **********************************************
+
+// Função Para Trocar os Banners
+
+function changeBanner() {
+  const banner1 = document.querySelector("[banner1]");
+  const banner2 = document.querySelector("[banner2]");
+  const banner3 = document.querySelector("[banner3]");
+
+  setInterval(() => {
+    if (banner1.classList.contains("active")) {
+      banner1.classList.remove("active");
+      banner1.classList.add("hidden");
+      banner2.classList.remove("hidden");
+      banner2.classList.add("active");
+    } else if (banner2.classList.contains("active")) {
+      banner2.classList.remove("active");
+      banner2.classList.add("hidden");
+      banner3.classList.remove("hidden");
+      banner3.classList.add("active");
+    } else if (banner3.classList.contains("active")) {
+      banner3.classList.remove("active");
+      banner3.classList.add("hidden");
+      banner1.classList.remove("hidden");
+      banner1.classList.add("active");
+    }
+  }, 6000);
+}
+
+changeBanner();
 
 //Função menu mobile
 
